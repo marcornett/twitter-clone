a1a9407aa169b5e1c9f5d61abf350f383562100f
Marcel Cornett <marcornett@gmail.com>
2020-12-01 21:18:46 -0500

    Notification views shows all notifications and will delete them upon view.

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Notifications
 
-# Create your views here.
+def notification_view(request):
+    notifications = Notifications.objects.all()
+    # TODO: if a get request
+    # TODO: if request.method == "GET":
+    # TODO: then delete notifications currently here
+    # TODO: notifications.delete()
+    context = {'notifications': notifications}
+    return render(request, 'notification/notifications.html', context)
+    
