c979fd5f6496c05e137eda61d45055ef56bee433
Marcel Cornett <marcornett@gmail.com>
2020-12-01 21:21:34 -0500

    Tweet model with user foreign key, tweet field, and date field.

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from twitteruser.models import TwitterUser
+from django.utils import timezone
 
-# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, null=True)
+    tweet = models.CharField(
+        max_length=140,
+        help_text='Tweets are limited to 140 characters.',
+        blank=True
+    )
+    date_created = models.DateTimeField(
+        default=timezone.now,
+        auto_now_add=False
+    )
