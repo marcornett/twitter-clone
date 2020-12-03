08933207b6d8f9389f19ad2543959b0fd3f442bd
Marcel Cornett <marcornett@gmail.com>
2020-12-02 21:34:11 -0500

    Notification view. This filters the notications for the current user, retrieves the message to be displayed and deletes all the tweets. This will show the tweet and on refresh will no longer show.

--- notification/views.py
+++ notification/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Notifications
+from django.contrib.auth.decorators import login_required
 
 def notification_view(request):
-    notifications = Notifications.objects.all()
-    # TODO: if a get request
-    # TODO: if request.method == "GET":
-    # TODO: then delete notifications currently here
-    # TODO: notifications.delete()
-    context = {'notifications': notifications}
+    notifications = Notifications.objects.filter(user=request.user)
+    tweets = []
+    for notification in notifications:
+        tweet = notification.notification.tweet
+        tweets.append(tweet)
+
+    notifications.delete()
+    context = {'tweets': tweets}
     return render(request, 'notification/notifications.html', context)
     
