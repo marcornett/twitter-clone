c00e09df0e6bbd676db10bebc02809c6d958bc16
Marcel Cornett <marcornett@gmail.com>
2020-12-02 21:36:07 -0500

    Tweet view has view to show one tweet. A view to create a tweet. The create tweet view also creates a notification and associates it with the user that recieves an @ in the tweet.

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tweet
+from .forms import TweetForm
+from twitteruser.models import TwitterUser
+from notification.models import Notifications
+from django.contrib.auth.decorators import login_required
+import re
 
-# Create your views here.
+def tweet_view(request, id):
+    tweet = Tweet.objects.get(id=id)
+    context = {'tweet': tweet}
+    return render(request, 'tweet/tweet.html', context)
+
+@login_required()
+def create_tweet(request):
+    form = TweetForm()
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            tweet = data['tweet']
+            pattern = re.compile(r'@(\w+)')
+            names = pattern.findall(tweet)
+            
+            tweet_model = Tweet.objects.create(
+                user=request.user,
+                tweet=data['tweet']
+            )
+            for name in names:
+                is_user = TwitterUser.objects.filter(display=name)
+                if is_user:
+                    user = TwitterUser.objects.get(display=name)
+                    Notifications.objects.create(
+                        user=user,
+                        notification=tweet_model
+                        )
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'authentication/generic_form.html', context)
