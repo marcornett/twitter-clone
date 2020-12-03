e2566d34f35d18c5b4fa86e3362e3eed56213fc4
Marcel Cornett <marcornett@gmail.com>
2020-12-02 21:38:51 -0500

    Urls from the authentication, twitter user, tweet, and notification apps.

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+    path('user/', include('twitteruser.urls')),
+    path('tweet/', include('tweet.urls')),
+    path('notifications/', include('notification.urls'))
 ]
