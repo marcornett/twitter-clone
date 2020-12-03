106e5de9a5e6799469910d5dfc9a2b706acb1358
Marcel Cornett <marcornett@gmail.com>
2020-12-03 12:24:44 -0500

    Added id and class to tweet text area tag.

--- tweet/forms.py
+++ tweet/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 from twitteruser.models import TwitterUser
 class TweetForm(forms.Form):
-    tweet = forms.CharField(max_length=140, widget=forms.Textarea)
+    tweet = forms.CharField(
+        max_length=140, 
+        widget=forms.Textarea(
+            attrs={
+                'id': 'textarea_field',
+                'class': 'nes-textarea'
+                }
+            ),
+        )
 
