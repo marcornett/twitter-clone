438773484037a36d32c11f42e4667f8f523cbe65
Marcel Cornett <marcornett@gmail.com>
2020-12-04 09:52:18 -0500

    Created get user function to find a user. Using this helper function in multiple places.

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render, redirect
-from tweet.models import Tweet
 from twitteruser.models import TwitterUser
 from django.contrib.auth.decorators import login_required
+from tweet.views import filter_tweets
+
 
 @login_required()
 def profile_view(request):
-    user = TwitterUser.objects.get(username=request.user.username)
-    tweets = Tweet.objects.filter(user=request.user).order_by('-date_created')
+    user = get_user(request.user.username)
+    tweets = filter_tweets(request.user).order_by('-date_created')
     num_of_following = len(user.follow.all())
     context = {
-        'tweets': tweets, 
+        'tweets': tweets,
         'user': user,
         'num_of_following': num_of_following,
         }
     return render(request, 'twitteruser/user.html', context)
 
+
 def user_view(request, username):
-    user = TwitterUser.objects.get(username=username)
-    tweets = Tweet.objects.filter(user=user)
+    user = get_user(username)
+    tweets = filter_tweets(user)
     follower = request.user.follow.filter(display=user.display)
     if request.method == 'POST':
-        current_user = TwitterUser.objects.get(username=request.user.username)
+        current_user = get_user(request.user.username)
         if len(follower) > 0:
             current_user.follow.remove(user)
         else:
@@ -32,3 +34,8 @@ def user_view(request, username):
         'follower': follower,
     }
     return render(request, 'twitteruser/user.html', context)
+
+
+def get_user(username):
+    user = TwitterUser.objects.get(username=username)
+    return user
